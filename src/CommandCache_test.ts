e49dbc64722d5d8dc0155d77e90c7222ec51abf2
UrielCh <uriel.chemouni@gmail.com>
2023-05-19 09:04:31 +0300

    fix unused import

--- src/CommandCache_test.ts
+++ src/CommandCache_test.ts
@@ -1,15 +1,17 @@
-import { assertEquals } from "../dev_deps.ts";
-/// import { KNOWN_METHODS } from "./CommandCache.ts";
+import { assertEquals, assert } from "../dev_deps.ts";
 import { getMidjourney } from "./Midjourney_test.ts";
 import { wait } from "./utils.ts";
 
-// Deno.test(async function LoadAllCmds() {
-//   const client = getMidjourney();
-//   const { commandCache } = client;
-//
-//   for (const mtd of ["describe"] as const) { // KNOWN_METHODS
-//     await wait(1000);
-//     const cmd = await commandCache.getCommand(mtd);
-//     assertEquals(cmd.name, mtd);
-//   }
-// });
+ Deno.test(async function LoadAllCmds() {
+   const client = getMidjourney();
+   if (!client) {
+        assert(1);
+        return;
+   }
+   const { commandCache } = client;
+   for (const mtd of ["describe"] as const) { // KNOWN_METHODS
+     await wait(1000);
+     const cmd = await commandCache.getCommand(mtd);
+     assertEquals(cmd.name, mtd);
+   }
+ });
