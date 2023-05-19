eb316140c41a11b18389d988ceb13b1c32ce098a
UrielCh <uriel.chemouni@gmail.com>
2023-05-19 09:05:13 +0300

    deno fmt

--- src/CommandCache_test.ts
+++ src/CommandCache_test.ts
@@ -1,17 +1,17 @@
-import { assertEquals, assert } from "../dev_deps.ts";
+import { assert, assertEquals } from "../dev_deps.ts";
 import { getMidjourney } from "./Midjourney_test.ts";
 import { wait } from "./utils.ts";
 
- Deno.test(async function LoadAllCmds() {
-   const client = getMidjourney();
-   if (!client) {
-        assert(1);
-        return;
-   }
-   const { commandCache } = client;
-   for (const mtd of ["describe"] as const) { // KNOWN_METHODS
-     await wait(1000);
-     const cmd = await commandCache.getCommand(mtd);
-     assertEquals(cmd.name, mtd);
-   }
- });
+Deno.test(async function LoadAllCmds() {
+  const client = getMidjourney();
+  if (!client) {
+    assert(1);
+    return;
+  }
+  const { commandCache } = client;
+  for (const mtd of ["describe"] as const) { // KNOWN_METHODS
+    await wait(1000);
+    const cmd = await commandCache.getCommand(mtd);
+    assertEquals(cmd.name, mtd);
+  }
+});
