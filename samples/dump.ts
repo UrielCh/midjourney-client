8d3dcfa14914ce35a2de3933522287d21a0e8113
urielch <uriel.chemouni@gmail.com>
2024-09-20 11:34:47 +0200

    update samples

---
+++ samples/dump.ts
@@ -0,0 +1,11 @@
+import { Midjourney } from "../mod.ts";
+
+/**
+ * Dump the auth token, guild id and channel id to the console,
+ * so they can be pasted into the environment variables
+ */
+const client = new Midjourney("interaction.txt");
+
+console.log(`export SALAI_TOKEN = "${client.auth}";`);
+console.log(`export SERVER_ID = "${client.guild_id}";`);
+console.log(`export CHANNEL_ID = "${client.channel_id}";`);
